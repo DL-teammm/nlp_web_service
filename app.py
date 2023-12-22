11d0c50cf9216e8d5a6ea7295a24ec897ad75e9a
full_metal0 <ilya.kalganov.973@gmail.com>
2023-12-22 04:21:02 +0300

    alpha build

---
+++ app.py
@@ -0,0 +1,91 @@
+import os
+import pandas as pd
+import streamlit as st
+
+def input_data(samples_path: str = 'samples_resume', user_path: str = 'user_resume') -> str:
+    samples = [
+        resume_name.replace('.csv', '') for resume_name in os.listdir(samples_path)
+    ]
+
+    row2_spacer1, row2_1, row2_spacer2 = st.columns((0.1, 3.2, 0.1))
+
+    with row2_1:
+        resume = st.selectbox(
+            "Select one of our sample resume",
+            tuple(samples),
+            index=None,
+            placeholder="Select sample resume...",
+        )
+
+        st.markdown("**or**")
+
+        uploaded_file = st.file_uploader("Pick a resume")
+        if uploaded_file is not None:
+            save_uploaded_file(uploaded_file, user_path)
+            resume = uploaded_file.name
+
+        if uploaded_file is None:
+            st.warning("""File should be in csv format""")
+
+        st.write('You selected:', resume)
+
+        if uploaded_file is None:
+            resume_path = os.path.join(
+                samples_path, f'{resume}.csv',
+            )
+        else:
+            resume_path = pd.read_csv(f'./{user_path}/{uploaded_file.name}')
+
+        return resume_path
+
+
+def save_uploaded_file(uploaded_file, user_resume):
+    with open(os.path.join(user_resume, uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    return st.success("Saved File: {} to user_resume".format(uploaded_file.name))
+
+
+def show_chain_results(
+# input_video_path: str
+):
+    # with st.spinner('Chain inference...'):
+    #     vacancies = chain.infer()
+
+    st.header('Recommended vacancies:')
+
+    df = pd.read_csv("./results/vacancies_example.csv")
+    header = [
+        "vacancy_name",
+        "full_company_name",
+        "regionName",
+        "salary",
+        "schedule_type",
+        "position_requirements",
+        "position_responsibilities",
+        ]
+    df.to_csv('./results/filtered_vacancies.csv', columns=header, index=False)
+
+    new_df = pd.read_csv("./results/filtered_vacancies.csv")
+
+    st.write(new_df)
+
+def build_page():
+    st.set_page_config(layout="wide")
+    st.title('Vacancy Recommendation Project')
+
+    row1_spacer1, row1_1, row1_spacer2 = st.columns((0.1, 3.2, 0.1))
+
+    with row1_1:
+        st.markdown(
+            " "
+        )
+        st.markdown(
+            ""
+        )
+
+    resume_path = input_data()
+
+    show_chain_results()
+
+if __name__ == '__main__':
+    build_page()
---
+++ chain.py
@@ -0,0 +1,38 @@
+import json
+import pandas as pd
+
+from preprocessing import *
+from variables import *
+
+
+class Chain:
+    def __init__(self, input_cv_path: str):
+        columns = ['title', 'city', 'area', 'desired_wage', 'education_level', 'languages', 'skills', 'about']
+        df_cv = pd.read_csv(input_cv_path)[columns].iloc[0]
+
+        line = ' '.join([str(item) for item in df_cv.values]).lower()
+        cv_doc = tokenize(line, stopwords=sw, need_lemmatize=True)
+        self.cv_emb = vectorize(fasttext, [cv_doc], vector_size=300)[0]
+
+    def infer(self):
+        with open('./database/cluster_centers.json') as json_file:
+            cluster_centers = json.load(json_file)
+
+        cos_dist = 0
+        cv_label = 0
+        for label, center in cluster_centers.items():
+            dist = cosine_similarity(self.cv_emb, center)
+            if dist > cos_dist:
+                cos_dist = dist
+                cv_label = label
+
+        with open('./database/cluster_samples.json', encoding='utf-8') as json_file:
+            cluster_samples = json.load(json_file)
+
+        return cluster_samples[str(cv_label)][:10]
+
+
+if __name__ == '__main__':
+    chain = Chain(input_cv_path='./test_vacs_cvs/resumes.csv') # внутри - обработка только первой файла-csv
+    out = chain.infer()
+    print(out)
